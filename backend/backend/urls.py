c934077e541c33d481d6b1cfe16ca57659ed3028
Santosh Varma Addala <santoshvarma2166@gmail.com>
2024-07-26 11:15:47 +0530

    change to base template

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from myapp.views import MyModelViewSet
 
-router = routers.DefaultRouter()
-router.register(r'mymodel', MyModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
